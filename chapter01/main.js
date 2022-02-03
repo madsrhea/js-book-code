ac69f69648340bd94bed0d946b3a3901a5017bd9
madsrhea <mads-axt@student.kirkwood.edu>
2022-02-03 14:58:28 -0600

    Class work

---
+++ chapter01/main.js
@@ -0,0 +1,8 @@
+function changeColor()
+{
+    document.body.style.backgroundColor = "black";
+    
+}
+
+var button = document.getElementById("clickMe");
+button.addEventListener("click",changeColor);
